8408596163375efa2b6f8af3c16d86e70f1b2f80
Bahram <baxram1997007@gmail.com>
2023-10-04 22:32:43 +0400

    added old homework tasks

---
+++ Patterms/Strategy/Proxy/Program.cs
@@ -0,0 +1,67 @@
+﻿Client client = new Client();
+Console.WriteLine("Client: Executing the client code with a real subject:");
+RealSubject realSubject = new RealSubject();
+client.ClientCode(realSubject);
+
+Console.WriteLine();
+
+Console.WriteLine("Client: Executing the same client code with a proxy:");
+Proxy proxy = new Proxy(realSubject);
+client.ClientCode(proxy);
+
+public interface ISubject
+{
+    void Request();
+}
+
+class RealSubject : ISubject
+{
+    public void Request()
+    {
+        Console.WriteLine("Реальный объект обработал запрос");
+    }
+}
+
+class Proxy : ISubject
+{
+    private RealSubject _realSubject;
+    private List<RealSubject> _logging;
+
+    public Proxy(RealSubject realSubject)
+    {
+        _realSubject = realSubject;
+    }
+
+    public void Request()
+    {
+        if (CheckAccess())
+        {
+            _realSubject.Request();
+
+            LogAccess();
+        }
+    }
+
+    private bool CheckAccess()
+    {
+        Console.WriteLine("Proxy: Проверка достоупа к реальному объекта");
+        return true;
+    }
+
+    private void LogAccess()
+    {
+        Console.WriteLine("Proxy: Сохранение доступа к реальному объекту");
+    }
+
+    public void ShowLog()
+    {
+    }
+}
+
+class Client
+{
+    public void ClientCode(ISubject subject)
+    {
+        subject.Request();
+    }
+}
